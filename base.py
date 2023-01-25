95d50659390566c2d1a49c3f8e416b00496cfe72
Leonard <lordknight1904@gmail.com>
2023-01-25 17:40:11 +0900

    finish rotation-ish?!?

--- base.py
+++ base.py
@@ -1,4 +1,3 @@
-import json
 from dataclasses import dataclass, field
 import numpy as np
 from typing import List
@@ -15,6 +14,18 @@ class Camera:
     sensor_size: np.ndarray=np.array([30, 30])
 
     @staticmethod
+    def qm(quaternion1, quaternion0):
+        w0, x0, y0, z0 = quaternion0
+        w1, x1, y1, z1 = quaternion1
+        
+        return np.array([
+            -x1*x0 - y1*y0 - z1*z0 + w1*w0,
+            x1*w0 - y1*z0 + z1*y0 + w1*x0,
+            x1*z0 + y1*w0 - z1*x0 + w1*y0,
+            -x1*y0 + y1*x0 + z1*w0 + w1*z0
+        ])
+
+    @staticmethod
     def _convert_to_ndarray(obj):
         if not isinstance(obj, (np.ndarray, np.generic)):
             return np.array(obj)
@@ -26,8 +37,9 @@ class Camera:
         self.resolution = self._convert_to_ndarray(self.resolution)
         self.sensor_size = self._convert_to_ndarray(self.sensor_size)
 
+        self._inv = np.array([1, -1, -1, -1])
+
     def world_to_camera(self, world_coor: List[np.ndarray]) -> List[np.ndarray]:
-        q = R.from_quat(self.quaternion)
-        camera_coor = q.apply(world_coor - self.position)
+        camera_coor = [self.qm(self.qm(self.quaternion, coor), self.quaternion*self._inv) for coor in world_coor]
 
         return world_coor
